c3ea0acece484f0dbe7548245e6ecbee80ab35dd
pecunofx <167554477+pecunofx@users.noreply.github.com>
2024-04-19 16:07:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// Importa el módulo 'os' para verificar la versión de Node.js
+const os = require('os');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+
+    // Verifica la versión de Node.js
+    if (os.version() !== 'v20.0.0') {
+        context.log('Por favor, actualiza a Node.js 20.');
+        return;
+    }
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
